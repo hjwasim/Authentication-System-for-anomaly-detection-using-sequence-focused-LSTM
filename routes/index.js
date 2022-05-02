7c6520b895dde1682ca7153b2fc03fe7bac2d0bb
Hjwasim <61841901+hjwasim@users.noreply.github.com>
2022-05-02 12:53:55 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Axway AI Fraud Detection' });
+  res.render('index', { title: 'Detecting Malign Requests using Hybrid Deep Learning Model with Sequence-based LSTM' });
 });
 
 module.exports = router;
